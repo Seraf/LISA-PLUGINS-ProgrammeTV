482a1a62ed5dbd48e7820e2fc3d6c1b3e77cf354
Seraf <julien.syx@gmail.com>
2013-07-06 18:01:08 +0200

    Changing the way the module is loaded

--- lang/fr/programmetv.rs
+++ lang/fr/programmetv.rs
@@ -5,12 +5,6 @@
 @ il y a quoi a la television ?
 
 > object getprogrammetv python
-    import sys, os, inspect
-    cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split( \
-        inspect.getfile(inspect.currentframe()))[0],os.path.normpath("Plugins/ProgrammeTV/modules/"))))
-    if cmd_subfolder not in sys.path:
-        sys.path.insert(0, cmd_subfolder)
-
-    from programmetv import ProgrammeTV
+    from ProgrammeTV.modules.programmetv import ProgrammeTV
     return ProgrammeTV().getProgrammeTV()
 < object
