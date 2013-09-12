d6bc83cad879401db9371972d0fd46344413c68d
Seraf <julien.syx@gmail.com>
2013-09-12 23:21:21 +0200

    adding lisa instance as a class object

--- modules/programmetv.py
+++ modules/programmetv.py
@@ -12,7 +12,8 @@ path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
 _ = translation = gettext.translation(domain='programmetv', localedir=path, languages=[configuration['lang']]).ugettext
 
 class ProgrammeTV:
-    def __init__(self):
+    def __init__(self, lisa):
+        self.lisa = lisa
         self.configuration_lisa = configuration
         mongo = MongoClient(self.configuration_lisa['database']['server'],
                             self.configuration_lisa['database']['port'])
