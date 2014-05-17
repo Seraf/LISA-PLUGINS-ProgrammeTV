aedb4b0bb729c4616b3384126dbb7a0ddea4b545
Julien Syx <julien.syx@gmail.com>
2014-05-17 20:30:52 +0200

    Removing LISA_PATH

--- lisa/plugins/ProgrammeTV/modules/programmetv.py
+++ lisa/plugins/ProgrammeTV/modules/programmetv.py
@@ -16,6 +16,7 @@ class ProgrammeTV(IPlugin):
             inspect.getfile(inspect.currentframe()))[0],os.path.normpath("../lang/"))))
         self._ = translation = gettext.translation(domain='programmetv',
                                                    localedir=self.path,
+                                                   fallback=True,
                                                    languages=[self.configuration_lisa['lang']]).ugettext
     def getProgrammeTV(self, jsonInput):
         self.downloadProgrammeTV()
